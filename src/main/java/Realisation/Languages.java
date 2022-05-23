56c382d9c0e31e5f69311e595b9fb96220dcad53
ShipIM <ilya.shipunov@gmail.com>
2022-05-23 22:33:43 +0300

    Locale

--- src/main/java/Realisation/Languages.java
+++ src/main/java/Realisation/Languages.java
@@ -34,6 +34,8 @@ public enum Languages implements Serializable {
         }
     };
 
+    public static final long serialVersionUID = 42L;
+
     private final String name;
 
     Languages(String name) {
